3fc56319a6f2b37670aa79db5cdc31b1288e78f9
pknowledge <abhishekrawane1195@gmail.com>
2022-01-02 15:09:38 +0530

    Added : Addition of Two Random Dice Digit Numbers

---
+++ SequenceProblem/TwoDiceNumber.js
@@ -0,0 +1,9 @@
+// 2 random dice number
+let diceNum1 = Math.floor(Math.random()*10)%6+1;
+console.log("Dice number 1 is : "+diceNum1);
+
+
+let diceNum2 = Math.floor(Math.random()*10)%6+1;
+console.log("Dice number 2 is : "+diceNum2);
+
+console.log("Addition of two random numbers is : "+(diceNum1 + diceNum2));
