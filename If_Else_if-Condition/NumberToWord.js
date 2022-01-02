8b5e2107de1e0f7550e55d5c2acde0d72ddab80e
pknowledge <abhishekrawane1195@gmail.com>
2022-01-03 00:14:55 +0530

    Updated : Printing Number To Word By using If_Else

--- If_Else_if-Condition/NumberToWord.js
+++ If_Else_if-Condition/NumberToWord.js
@@ -1,36 +1,35 @@
-let number = Math.floor(Math.random() * 10)%10;
-console.log("Random Number: " + number);
-switch (number) {
-    case 0:
-        console.log("Number 0 in word is Zero");
-        break;
-    case 1:
-        console.log("Number 1 in word is One");
-        break;
-    case 2:
-        console.log("Number 2 in word is Two");
-        break;
-    case 3:
-        console.log("Number 3 in word is Three");
-        break;
-    case 4:
-        console.log("Number 4 in word is Four");
-        break;
-    case 5:
-        console.log("Number 5 in word is Five");
-        break;
-    case 6:
-        console.log("Number 6 in word is Six");
-        break;
-    case 7:
-        console.log("Number 7 in word is Seven");
-        break;
-    case 8:
-        console.log("Number 8 in word is Eight");
-        break;
-        case 9:
-        console.log("Number 8 in word is nine");
-        break;
-    default:
-        console.log("Enter the number between 0 to 9");
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let number = prompt("Enter number to display number in word : ");
+
+if(number==1){
+    console.log("One");
+}
+else if(number ==2){
+    console.log("Two");
+}
+else if(number ==3){
+    console.log("Three");
+}
+else if(number ==4){
+    console.log("Four");
+}
+else if(number ==5){
+    console.log("Five");
+}
+else if(number ==6){
+    console.log("Six");
+}
+else if(number ==7){
+    console.log("Seven");
+}
+else if(number ==8){
+    console.log("Eight");
+}
+else if(number ==9){
+    console.log("Nine");
+}
+else if(number ==10){
+    console.log("Ten");
 }
